5a6ebf640b6334bfb08306748033e624f299ea3d
Fabriciodossantos <fsan4344@gmail.com>
2023-02-08 20:01:12 -0300

    ..

--- src/Conta.php
+++ src/Conta.php
@@ -4,24 +4,40 @@ class Conta
 {
     public string $cpfTitular;
     public string $nomeTitular;
-    public float $saldoConta;
+    public float $saldoConta = 0;
 
     public function sacar ($valorSacar):void
     {
         if ($valorSacar > $this -> saldoConta) {
             echo 'Saldo insuficiente';
-        } else {
-            $this -> saldoConta -= $valorSacar;
+            return;
         }
+        
+        $this -> saldoConta -= $valorSacar;
     }
+
 
 
     public function depositar ($valorDeposito):void
     {
         if ($valorDeposito < 0) {
             echo 'O valor precisa ser positivo';
-        } else {
-            $this -> saldoConta += $valorDeposito;
+            return;
         }
+
+        $this -> saldoConta += $valorDeposito;
     }
+    
+
+    public function transferir ($valorATransferir, Conta $contaDeDestino):void
+    {
+        if ($valorATransferir > $this -> saldoConta) {
+            echo 'Saldo insuficiente!';
+            return;
+        }
+
+        $this -> sacar($valorATransferir);
+        $contaDeDestino -> saldoConta += $valorATransferir;
+    }
+    
 }
